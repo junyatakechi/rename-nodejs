aca574ddf3a0e598480f8f88b028d3f18993d1a2
junyatakechi <takechi48j@gmail.com>
2023-03-12 12:21:29 +0900

    追加: マルチ環境変数ファイル

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,3 +1,10 @@
+// マルチ環境変数ファイル
+const dotenv = require("dotenv");
+dotenv.config({override: true});
+if(process.env.MODE){
+  console.log("MODE:", process.env.MODE);
+  dotenv.config({path: `.env.${process.env.MODE}`, override: true});
+}
 
 //
 const main = async function(){
